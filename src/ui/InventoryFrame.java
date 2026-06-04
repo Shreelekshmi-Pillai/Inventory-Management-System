abbbee3f08b1d4b6b8eb13fc47b7274cfa23104f
Shreelekshmi-Pillai <shreemalu007@gmail.com>
2026-06-04 13:15:16 +0530

    Add files via upload

---
+++ src/ui/InventoryFrame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ui;
+
+/**
+ * 
+ */
+public class InventoryFrame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
